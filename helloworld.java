8009305ef8a7f1dd987451f9e272fc5e9b4dfd66
tharunsajjan <68546601+tharunsajjan@users.noreply.github.com>
2022-03-01 19:20:47 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld{
   public static void main(String[] args){
-     System.out.println("Welcome to Jenkins Workshop. This is Poll SCM every minute");
+     System.out.println("Welcome to Jenkins Workshop. This is Poll SCM every minute. This is second change");
   }
 } 
